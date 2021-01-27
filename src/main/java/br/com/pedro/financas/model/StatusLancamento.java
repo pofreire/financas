4e0237621d88ebc7e02f8aff92f5f35b17fe1f02
Pedro Freire <30freire@gmail.com>
2021-01-27 14:48:24 -0300

    projeto de estudo - spring

---
+++ src/main/java/br/com/pedro/financas/model/StatusLancamento.java
@@ -0,0 +1,7 @@
+package br.com.pedro.financas.model;
+
+public enum StatusLancamento {
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+}
---
+++ src/main/java/br/com/pedro/financas/model/TipoLancamento.java
@@ -0,0 +1,6 @@
+package br.com.pedro.financas.model;
+
+public enum TipoLancamento {
+	RECEITA,
+	DESPESA
+}
